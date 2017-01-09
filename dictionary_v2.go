8518a43000400641c7e7a2afabe01ee5360c44f8
Simon Critchley <si@avocet.io>
2017-01-09 16:07:09 +0000

    dictionary_v2.go: update implementation

--- dictionary_v2.go
+++ dictionary_v2.go
@@ -7,18 +7,18 @@ import (
 // Dictionary is a data structure that holds a distinct set of string values.
 type DictionaryV2 struct {
 	values    []string
-	valuesMap map[string]struct{}
+	valuesMap map[string]int
 }
 
 // NewDictionaryV2 returns a new DictionaryV2 intialised with the provided initialCapacity.
 func NewDictionaryV2() *DictionaryV2 {
 	return &DictionaryV2{
-		valuesMap: make(map[string]struct{}),
+		valuesMap: make(map[string]int),
 	}
 }
 
 func (d *DictionaryV2) add(value string) {
-	d.valuesMap[value] = struct{}{}
+	d.valuesMap[value] = 0
 }
 
 func (d *DictionaryV2) prepare() {
@@ -27,18 +27,19 @@ func (d *DictionaryV2) prepare() {
 		d.values = append(d.values, value)
 	}
 	sort.Strings(d.values)
+	for i := range d.values {
+		d.valuesMap[d.values[i]] = i
+	}
 }
 
 func (d *DictionaryV2) get(value string) (int, bool) {
-	i := sort.SearchStrings(d.values, value)
-	if i < len(d.values) && d.values[i] == value {
-		return i, true
+	if ind, ok := d.valuesMap[value]; ok {
+		return ind, true
 	}
 	return -1, false
 }
 
 func (d *DictionaryV2) forEach(fn func(value string) error) error {
-	d.prepare()
 	for _, value := range d.values {
 		err := fn(value)
 		if err != nil {
@@ -49,7 +50,7 @@ func (d *DictionaryV2) forEach(fn func(value string) error) error {
 }
 
 func (d *DictionaryV2) reset() {
-	d.valuesMap = make(map[string]struct{})
+	d.valuesMap = make(map[string]int)
 	d.values = nil
 }
 
